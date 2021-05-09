a835de918413d9a86a28ca3269824d9384912a47
Daniel Hongwoo <daniel.hongwoo.lee@gmail.com>
2021-05-09 22:35:22 +0900

    make one - bottom-up

--- one/one.cpp
+++ one/one.cpp
@@ -7,23 +7,17 @@ bool IsDevidable(int num, int denom) {
   return num % denom == 0;
 }
 
-std::map<int, int> m = {{1, 0}, {2, 1}, {3, 1}, {5, 1}};
-
 int GetOpCount(int num) {
-  if (m.find(num) != m.end()) {
-    return m.at(num);
+  std::map<int, int> m = {{1, 0}, {2, 1}, {3, 1}, {5, 1}};
+  for (int i = 2; i <= num; i++) {
+    m[i] = m[i-1] + 1;
+    if (IsDevidable(i, 5)) {
+      m[i] = std::min(m[i], m[i/5] + 1);
+    } else if (IsDevidable(i, 3)) {
+      m[i] = std::min(m[i], m[i/3] + 1);
+    } else if (IsDevidable(i, 2)) {
+      m[i] = std::min(m[i], m[i/2] + 1);
+    }
   }
-
-  auto minus = GetOpCount(num - 1);
-  int div = 1000000;
-  if (IsDevidable(num, 5)) {
-    div = GetOpCount(num / 5);
-  } else if (IsDevidable(num, 3)) {
-    div = GetOpCount(num / 3);
-  } else if (IsDevidable(num, 2)) {
-    div = GetOpCount(num / 2);
-  }
-  auto count = std::min(minus, div) + 1;
-  m[num] = count;
-  return count;
+  return m[num];
 }
